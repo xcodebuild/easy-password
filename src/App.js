32147d2382a3aa5a91c2d105a7b5848c86b8cae0
当轩 <code.falling@gmail.com>
2017-01-20 23:35:25 +0800

    copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import './App.css';
+import Clipboard from 'clipboard';
 
 //import styles
 import 'weui';
@@ -15,21 +15,63 @@ import {
   CellsTitle, 
   Form,
   FormCell,
-  CellHeader,
   Label,
-  CellBody,
   Input,
-  Button
+  Button,
+  Toast
 } from 'react-weui';
 
 class App extends Component {
- 
+  state = {
+    memory: null,
+    code: null,
+    showToast: false,
+    toastCopySuccess: false,
+    toastTimer: null,
+  };
+
+  updateState(newPart) {
+    this.setState(Object.assign(this.state, newPart));
+  }
+
+  componentDidMount() {
+    const TOAST_TIME = 2000;
+    const state = this.state;
+    const clipboard = new Clipboard('#password-to-copy');
+
+    const showToast = (success: boolean): void => {
+      if (!state.toastTimer) {
+        clearTimeout(state.toastTimer);
+      }
+      this.updateState({
+        showToast: true,
+        toastCopySuccess: success,
+        toastTimer: setTimeout(() => {
+          this.updateState({
+            toastTimer: null,
+            showToast: false,
+          });
+        }, TOAST_TIME),
+      });
+    }
+    clipboard.on('success', e => {
+      showToast(true);
+    });
+
+    clipboard.on('error', e => {
+      showToast(false);
+    });
+  }
+
   render() {
-    const state = this.state || {};
+    const state = this.state;
     const buildChangeHandler = (stateName: string) => e => this.setState(Object.assign(state, {[stateName]: e.target.value}));
 
     return (
       <Page title='EasyPassword' subtitle='Easy way to manage password'>
+        <Toast icon={state.toastCopySuccess ? 'success-no-circle': 'error'} show={this.state.showToast}>
+          {state.toastCopySuccess ? 'Copied' : 'Error'}
+        </Toast>
         <CellsTitle></CellsTitle>
         <Form>
           <FormCell>
@@ -49,7 +91,7 @@ class App extends Component {
             </cellbody>
           </FormCell>
         </Form>
-        <Button className={style.copyButton}>
+        <Button id='password-to-copy' data-clipboard-target="#password-to-copy" className={style.copyButton}>
           {!(state.code && state.memory) ? 'Click to Copy' : encrypt(state.memory, state.code)}
         </Button>
       </Page>
